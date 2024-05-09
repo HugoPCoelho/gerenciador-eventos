c360c24cbc7e29bdbe343524a3b750101fea5ca9
HugoPCoelho <hugo.penha.coelho@gmail.com>
2024-05-09 14:27:22 -0300

    Created event REST resources.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { CategoriaModule } from './categoria/categoria.module';
 import { LocalModule } from './local/local.module';
+import { EventoModule } from './evento/evento.module';
 
 @Module({
-  imports: [PrismaModule, CategoriaModule, LocalModule],
+  imports: [PrismaModule, CategoriaModule, LocalModule, EventoModule],
   controllers: [AppController],
   providers: [AppService],
 })
